c15a72c4400fb48c7e0ea5aeda8505791826863c
yanfi-sher <loss.soul20@gmail.com>
2024-09-26 16:53:05 +0300

    init commit

---
+++ src/main/java/telran/interview/Connection.java
@@ -0,0 +1,14 @@
+package telran.interview;
+
+public class Connection {
+    private String connectionId;
+
+    public Connection(String connectionId) {
+        this.connectionId = connectionId;
+    }
+
+    public String getConnectionId() {
+        return connectionId;
+    }
+
+}
